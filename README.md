f7f95482dbf1a9faf35463eb8606fe37e7279c6c
ej-shafran <ejshafran@hilma.tech>
2023-10-23 06:41:58 +0300

    feat: implement actual parser

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csv-parser
+
+A simple CSV parser in Haskell, using Parsec!
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,64 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
+import Text.Parsec
+import Text.Parsec.String
+import qualified Data.List.NonEmpty as NE
+import Data.List.NonEmpty (NonEmpty((:|)))
+import Control.Monad (guard)
+import Data.Function (on)
+
+lf :: Parser Char
+lf = char '\n'
+
+cr :: Parser Char
+cr = char '\r'
+
+lineEnd :: Parser String
+lineEnd = (pure <$> lf) <|> (pure <$> cr) <|> ((:) <$> cr <*> (pure <$> lf))
+
+comma :: Parser Char
+comma = char ','
+
+dquote :: Parser Char
+dquote = char '"'
+
+textData :: Parser Char
+textData = oneOf $ " !#$%&'()*+-./:;<=>?@[\\]^_`{|}~" ++ ['0'..'9'] ++ ['A' .. 'Z'] ++ ['a'..'z']
+
+nonEscaped :: Parser String
+nonEscaped = many textData
+
+escaped :: Parser String
+escaped = dquote *> many (textData <|> comma <|> cr <|> lf <|> try (dquote *> dquote)) <* dquote
+
+field :: Parser String
+field = escaped <|> nonEscaped
+
+name :: Parser String
+name = field
+
+record :: Parser [String]
+record = (:) <$> field <*> many (comma *> field)
+
+header :: Parser [String]
+header = (:) <$> name <*> many (comma *> name)
+
+data CSVFile = CSVFile { fileHeader :: Maybe [String], fileRecords :: NE.NonEmpty [String] } deriving Show
+
+file :: Parser CSVFile
+file = do
+  fileHeader <- optionMaybe $ try (header <* lineEnd)
+  first <- record
+  rest <- many $ try $ lineEnd *> record
+  optional lineEnd
+  guard (all (((==) `on` length) first) rest) <?> "identical record lengths"
+  let fileRecords = first :| rest
+  return CSVFile { fileHeader, fileRecords }
+
+rstrip :: String -> String
+rstrip = reverse . dropWhile (=='\n') . reverse
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = interact $ show . parse file "STDIN" . reverse
+
